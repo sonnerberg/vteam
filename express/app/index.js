ced637520001962d8f47655468ec6a6cb3460787
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2022-11-24 13:25:36 +0100

    Add folder structure for different versions

--- express/app/index.js
+++ express/app/index.js
@@ -1,10 +1,15 @@
 const app = require('./app'); // the actual Express application
 const http = require('http');
 
-const server = http.createServer(app);
+const v1 = require('./v1/index.js');
+const v2 = require('./v2/index.js');
 
 const PORT = 3000;
 
+app.use('/v1', v1);
+app.use('/v2', v2);
+
+const server = http.createServer(app);
 server.listen(PORT, () => {
     console.info(`Server running on port ${PORT}`);
 });
