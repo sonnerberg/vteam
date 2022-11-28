e250d7a0a037e9633599e7cd213470556bfb4b86
tzwo <tomzwo@gmail.com>
2022-11-28 17:55:33 +0100

    Add route to get scooters

--- express/app/v1/index.js
+++ express/app/v1/index.js
@@ -10,4 +10,10 @@ router.get('/mariadb', async (req, res) => {
     res.status(200).json(data);
 });
 
+router.get('/scooter', async (req, res) => {
+    const sql = 'SELECT * FROM scooter';
+    const data = await queryDatabase(sql);
+    res.status(200).json(data);
+});
+
 module.exports = router;
