c16562e11ade8b512a7e0c2570e73eb28c7b9bde
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-29 16:18:11 +0100

    Add comments add title

--- react/app/src/components/LayerAccordion.js
+++ react/app/src/components/LayerAccordion.js
@@ -3,25 +3,28 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
+import React from 'react';
 
+/**
+ *
+ * @param {object} props - Props for the function
+ * @param {string} props.title - Title to display on the accordion
+ * @param {React.ReactElement} card - Card with info
+ * @returns {React.ReactElement} - The accordion
+ */
 const LayerAccordion = (props) => {
     return (
         <div>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography>
-                        {props.title}
-                    </Typography>
+            <Accordion defaultExpanded={props.expanded}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{props.title}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <div>{props.card}</div>
                 </AccordionDetails>
             </Accordion>
         </div>
-
     );
-}
+};
 
 export default LayerAccordion;
