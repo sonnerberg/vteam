0e7995f5691b74d183f92ec6bee7dacc943171cd
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-29 16:27:05 +0100

    Add onchange function and comments

--- react/app/src/components/LayerSwitch.js
+++ react/app/src/components/LayerSwitch.js
@@ -1,9 +1,20 @@
 import { Switch } from '@mui/material';
 
-const LayerSwitch = (props) => {
 
+/**
+ * A switch
+ * @param {object} props - Props for the function
+ * @param {function} props.setShowLayer - Function to call when switch i switched
+ * @param {boolean} props.showLayer - Calling the setShow function sets this to !value when passed in
+ * @returns {React.ReactElement} - The switch
+ */
+
+const LayerSwitch = (props) => {
+    function setShowLayer () {
+        props.setShowLayer(!props.showLayer);
+    }
     return (
-        <Switch></Switch>
+        <Switch onChange={setShowLayer}></Switch>
     );
 }
 
