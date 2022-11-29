9cd7173c648ed61fa026fa84068b6db35c0e0ec9
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-29 16:16:19 +0100

    Restore App.js to react app default

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,62 +1,25 @@
-import './App.css';
-import LayerStack from './components/LayerStack';
-import LayerAccordion from './components/LayerAccordion';
-import LayerButton from './components/LayerButton';
-import LayerButtonGroup from './components/LayerButtonGroup';
-import LayerContainer from './components/LayerContainer';
-import LayerSwitch from  './components/LayerSwitch';
-import LayerCard from './components/LayerCard';
-import LayerGrid from './components/LayerGrid';
+import './App.css'
+import logo from './logo.svg'
 
 function App() {
-   const city = {
-        "id": 1,
-        "name": "Eskilstuna",
-        "bla": "Bla",
-        "position": {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [
-            [
-              [59.4128,16.4246], [59.4308,16.4747], [59.3760,16.5876],
-              [59.3138,16.4743], [59.4128,16.4246]
-            ]
-          ]
-        },
-        "properties": {
-          "prop0": "value0",
-          "prop1": { "this": "that" }
-        }
-      }
-    }
-
-    const cardButton = <LayerButton buttonText={"Edit"} />
-    const card = <LayerCard content={city} button={cardButton}/>
-    const accordion = <LayerAccordion title={"Eskilstuna"} card={card}/>
-    const layerSwitch = <LayerSwitch />
-    const editButton = <LayerButton buttonText={"Ny"} size={"small"} />
-    const searchButton = <LayerButton buttonText={"Sök"} size={"small"}/>
-    const buttonGroup = <LayerButtonGroup buttons={[editButton, searchButton]} />
-    const container = <LayerGrid switch={layerSwitch} accordion={accordion} buttonGroup={buttonGroup}/>
-    /* const container = <LayerContainer grid={grid} />
- */
-    const containerArray = [container];
-
-
     return (
-    <div className="App">
-        <header className="App-header">
-        {/* <LayerButtonGroup buttons={[editButton, searchButton]} /> */}
-        <LayerStack components={containerArray}/>
-        {/* <LayerButton buttonText={"Search"} />
-        <LayerButton buttonText={"Click me"} />
-        <LayerButton buttonText={"Edit"} />
-        <LayerCard content={"Bike1"} button={cardButton}/>
-        <LayerAccordion title={"Cities"} card={card}/> */}
-        </header>
-    </div>
-    );
-    }
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
+}
 
-export default App;
+export default App
