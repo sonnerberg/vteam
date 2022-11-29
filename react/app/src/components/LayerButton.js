1b567d01cfb98c7f1aa4572e0cab301dfc32ecc5
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-29 16:18:58 +0100

    Add commnets

--- react/app/src/components/LayerButton.js
+++ react/app/src/components/LayerButton.js
@@ -1,8 +1,18 @@
 import { Button } from '@mui/material';
 
+
+/**
+ * A button
+ * @param {object} props - Props for the function
+ * @param {function} props.handleClick- Functon to execute when button is pressed
+ * @param {string} props.size - The size of the button
+ * @param {number} props.width - The width of the button
+ * @returns {React.ReactElement} - The button
+ */
 const LayerButton = (props) => {
+
     return (
-        <Button onClick={props.handleClick} size={props.size}>{props.buttonText}</Button>
+        <Button onClick={props.handleClick} size={props.size} sx={{ width: props.width }}>{props.buttonText}</Button>
     );
 }
 
