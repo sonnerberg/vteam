b59b503705a4b953747664771dd9b7efc30386bf
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-29 16:26:29 +0100

    Change spacing add comments

--- react/app/src/components/LayerStack.js
+++ react/app/src/components/LayerStack.js
@@ -1,9 +1,16 @@
 import { Stack } from '@mui/material';
 
+
+/**
+ * A stack with React components
+ * @param {object} props - Props for the function
+ * @param {React.ReactElement[]} props.components - The components which should be added to the stack
+ * @returns {React.ReactElement} - The stack
+ */
 const LayerStack = (props) => {
 
     return (
-        <Stack direction="column" spacing={2}>
+        <Stack direction="column" spacing={0}>
             {props.components.map((component, index) =>
                 <div key={index}>{component}</div>
             )}
