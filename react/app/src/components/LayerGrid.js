bdb1885f2dde2c79abdd48a728dfee6c36afe48e
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-30 13:34:41 +0100

    Remove paper, change layout

--- react/app/src/components/LayerGrid.js
+++ react/app/src/components/LayerGrid.js
@@ -2,7 +2,6 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import React from 'react';
 
-
 /**
  * A grid to display a switch, an accordion and a button group
  * @param {object} props - Props for the function
@@ -12,24 +11,22 @@ import React from 'react';
  * @returns {React.ReactElement} - The grid
  */
 const LayerGrid = (props) => {
-    console.log(props)
-    return(
-        <Paper elevation={3}>
-                <Grid container spacing={1} direction="row"  margin={2}>
-                    <Grid >
-                        <div>{props.switch}</div>
-                    </Grid>
+    console.log(props);
+    return (
+        <div>
+            <Grid container spacing={1} direction="row">
+                <Grid item xs={1} margin={1}>
+                    <div>{props.switch}</div>
                 </Grid>
-                <Grid container spacing={1} direction="row"  >
-                    <Grid item xs={10}  margin={1}>
-                            <div>{props.accordion}</div>
-                    </Grid>
-                    <Grid item xs={1} mt={0.5} mb={0.5}>
-                        <div>{props.buttonGroup}</div>
-                    </Grid>
+                <Grid item xs={8} margin={1}>
+                    <div>{props.accordion}</div>
                 </Grid>
-        </Paper>
+                <Grid item xs={1} mt={0.5} mb={0.5}>
+                    <div>{props.buttonGroup}</div>
+                </Grid>
+            </Grid>
+        </div>
     );
-}
+};
 
 export default LayerGrid;
