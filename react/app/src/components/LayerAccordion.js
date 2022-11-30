f7de0b1f8bcf89c7a743d241dfc9f59e75aeb3be
Joel Löfgren <jdtlofgren@gmail.com>
2022-11-30 21:14:40 +0100

    Add use of eventbus to listen for click events in map and display info in accordion

--- react/app/src/components/LayerAccordion.js
+++ react/app/src/components/LayerAccordion.js
@@ -3,7 +3,10 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
-import React from 'react';
+import eventBus from '../models/eventBus';
+import { useEffect, useState, useRef } from 'react';
+import LayerCard from './LayerCard';
+import LayerButton from './LayerButton';
 
 /**
  *
@@ -13,7 +16,9 @@ import React from 'react';
  * @returns {React.ReactElement} - The accordion
  */
 const LayerAccordion = (props) => {
-    return (
+    const initialProps = useRef(props);
+
+    const [ accordion, setAccordion] = useState (
         <div>
             <Accordion expanded={props.expanded}>
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
@@ -24,7 +29,37 @@ const LayerAccordion = (props) => {
                 </AccordionDetails>
             </Accordion>
         </div>
+    )
+
+    useEffect(() => {
+        eventBus.on(props.event, (data) => {
+            const editButton = (
+                <LayerButton buttonText={'Ändra'} size={'small'} width={25} />
+            );
+            const card = <LayerCard content={data} button={editButton} />
+            const newAccordion = (
+                <div>
+                    <Accordion expanded={true}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>{initialProps.current.title}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <div>{card}</div>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+            );
+
+            setAccordion(newAccordion);
+        });
+
+        return eventBus.remove(props.event);
+    }, [])
+
+    return (
+        accordion
     );
+
 };
 
 export default LayerAccordion;
