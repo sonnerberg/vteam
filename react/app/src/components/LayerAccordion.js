090d02bdc15eade0d1acc2792c2237aa8d2de9f2
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-01 12:38:17 +0100

    Add possibilty to de-expand accordion, only create new card when click event in map fires

--- react/app/src/components/LayerAccordion.js
+++ react/app/src/components/LayerAccordion.js
@@ -4,7 +4,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 import eventBus from '../models/eventBus';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import LayerCard from './LayerCard';
 import LayerButton from './LayerButton';
 
@@ -16,48 +16,37 @@ import LayerButton from './LayerButton';
  * @returns {React.ReactElement} - The accordion
  */
 const LayerAccordion = (props) => {
-    const initialProps = useRef(props);
+    const [card, setCard] = useState(null);
+    const [expanded, setExpanded] = useState(false);
 
-    const [ accordion, setAccordion] = useState (
-        <div>
-            <Accordion expanded={props.expanded}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>{props.title}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <div>{props.card}</div>
-                </AccordionDetails>
-            </Accordion>
-        </div>
-    )
+    const handleChange = () => {
+        setExpanded(!expanded);
+      };
 
     useEffect(() => {
         eventBus.on(props.event, (data) => {
             const editButton = (
                 <LayerButton buttonText={'Ändra'} size={'small'} width={25} />
             );
-            const card = <LayerCard content={data} button={editButton} />
-            const newAccordion = (
-                <div>
-                    <Accordion expanded={true}>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                            <Typography>{initialProps.current.title}</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <div>{card}</div>
-                        </AccordionDetails>
-                    </Accordion>
-                </div>
-            );
-
-            setAccordion(newAccordion);
+            const newCard = <LayerCard content={data} button={editButton} />
+            setCard(newCard);
+            setExpanded(true);
         });
 
         return eventBus.remove(props.event);
     }, [])
 
     return (
-        accordion
+        <div>
+            <Accordion expanded={expanded} onChange={handleChange}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{props.title}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {card ? <div>{card}</div> : <></>}
+                </AccordionDetails>
+            </Accordion>
+        </div>
     );
 
 };
