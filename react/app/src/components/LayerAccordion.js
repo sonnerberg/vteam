76f3c93231226edcdd91c8e177c76fa6e7b4ffa0
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-01 19:14:46 +0100

    Add functionality to show form card

--- react/app/src/components/LayerAccordion.js
+++ react/app/src/components/LayerAccordion.js
@@ -4,9 +4,10 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 import eventBus from '../models/eventBus';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import LayerCard from './LayerCard';
 import LayerButton from './LayerButton';
+import LayerFormCard from './LayerFormCard';
 
 /**
  *
@@ -17,24 +18,61 @@ import LayerButton from './LayerButton';
  */
 const LayerAccordion = (props) => {
     const [card, setCard] = useState(null);
+    const [formCard, setFormCard] = useState(null);
+    const [showFormCard, setShowFormCard] = useState(false);
     const [expanded, setExpanded] = useState(false);
+
+    console.log('formcard', formCard);
+
+    console.log('showform', showFormCard);
 
     const handleChange = () => {
         setExpanded(!expanded);
-      };
+    };
+
+    const handleClickChangeButton = () => {
+        console.log('Ändra');
+        console.log('showforminclick', showFormCard);
+        setShowFormCard(true);
+    };
+
+    const handleClickCancelButton = () => {
+        console.log('Cancel');
+        console.log('showformincancel', showFormCard);
+        setShowFormCard(false);
+    };
 
     useEffect(() => {
         eventBus.on(props.event, (data) => {
+            console.log('data in useeffect', data);
             const editButton = (
-                <LayerButton buttonText={'Ändra'} size={'small'} width={25} />
+                <LayerButton
+                    buttonText={'Ändra'}
+                    size={'small'}
+                    width={25}
+                    handleClick={handleClickChangeButton}
+                />
             );
-            const newCard = <LayerCard content={data} button={editButton} />
+
+            const cancelButton = (
+                <LayerButton
+                    buttonText={'Avbryt'}
+                    size={'small'}
+                    width={25}
+                    handleClick={handleClickCancelButton}
+                />
+            );
+            const newCard = <LayerCard content={data} button={editButton} />;
+            const newFormCard = (
+                <LayerFormCard content={data} button={cancelButton} />
+            );
             setCard(newCard);
+            setFormCard(newFormCard);
             setExpanded(true);
         });
 
         return eventBus.remove(props.event);
-    }, [])
+    }, []);
 
     return (
         <div>
@@ -43,12 +81,19 @@ const LayerAccordion = (props) => {
                     <Typography>{props.title}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    {card ? <div>{card}</div> : <></>}
+                    {(() => {
+                        if (card) {
+                            if (showFormCard) {
+                                return formCard;
+                            }
+
+                            return card;
+                        }
+                    })()}
                 </AccordionDetails>
             </Accordion>
         </div>
     );
-
 };
 
 export default LayerAccordion;
