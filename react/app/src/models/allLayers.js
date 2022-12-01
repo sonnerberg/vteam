cb13b22f172b104ed3080d0da56897e280515e58
patrikmalvenius <malvenius@gmail.com>
2022-12-01 13:01:24 +0100

    add 1000 points to map

--- react/app/src/models/allLayers.js
+++ react/app/src/models/allLayers.js
@@ -1,4 +1,4 @@
-import L from 'leaflet'
+import L from 'leaflet';
 
 const allLayers = {
     //vi kör featuregroup pga lättast då att binda funktioner till alla medlemmar (kartobjekt) i gruppen
@@ -7,21 +7,21 @@ const allLayers = {
     cities: L.featureGroup()
         .bindPopup('Hello from city!')
         .on('click', function () {
-            alert('Clicked on a member of the city group!')
+            alert('Clicked on a member of the city group!');
         }),
     chargingStations: L.featureGroup().on('click', function (e) {
-        const allObjects = this.getLayers()
-        allObjects.forEach((object) => console.log('OBJECT ', object))
-        console.log('E TARGET', e.target)
-        console.log('THIS', this)
+        const allObjects = this.getLayers();
+        allObjects.forEach((object) => console.log('OBJECT ', object));
+        console.log('E TARGET', e.target);
+        console.log('THIS', this);
         allObjects.forEach((object) =>
             alert('YOU CLICKED OBJECT ID ' + object.backendId)
-        )
+        );
     }),
     parkingLots: L.featureGroup()
         .bindPopup('Hello from parkinglot!')
         .on('click', function () {
-            alert('Clicked on a member of the parkinglot group!')
+            alert('Clicked on a member of the parkinglot group!');
         }),
     bikes: L.featureGroup().on('click', function (event) {
         alert(
@@ -30,18 +30,21 @@ const allLayers = {
             //other than attaching a listner to every object when its inserted
             //in the featuregroup but that seems insane with 1000 bikes
             `BIKE NO ${event.layer.backendId} IS IT RENTED? ${event.layer.rented}`
-        )
+        );
     }),
     workshops: L.featureGroup()
         .bindPopup('Hello from workshop!')
         .on('click', function () {
-            alert('Clicked on a member of the workshop group!')
+            alert('Clicked on a member of the workshop group!');
         }),
     zones: L.featureGroup()
         .bindPopup('Hello from zones!')
         .on('click', function () {
-            alert('Clicked on a member of the zones group!')
+            alert('Clicked on a member of the zones group!');
         }),
-}
+    points: L.featureGroup().on('click', function () {
+        alert('Clicked on a member of the 1000 points group!');
+    }),
+};
 
-export default allLayers
+export default allLayers;
