7b062914a226a537f86e17b57caa193fd14158c5
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2022-12-03 20:07:50 +0100

    Add super column and stored procedures

--- mariadb/admin.sql
+++ mariadb/admin.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS `admin`;
 
 CREATE TABLE IF NOT EXISTS `admin` (
 `password` VARCHAR(255),
-`email` VARCHAR(50),
+`email` VARCHAR(100),
+`super` BOOLEAN,
 PRIMARY KEY (`email`))
 ENGINE = InnoDB
 CHARSET utf8
@@ -10,7 +11,7 @@ COLLATE utf8_swedish_ci
 ;
 
 --
--- Insert an admin
+-- Insert admins
 --
 LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/init_data/admins.csv'
 INTO TABLE admin
@@ -21,7 +22,7 @@ FIELDS
 LINES
     TERMINATED BY '\n'
 IGNORE 1 LINES
-(email, password)
+(email, password, super)
 ;
 
 -- Procedure get_filtered_admin()
@@ -37,6 +38,50 @@ CREATE PROCEDURE get_filtered_admin(
 
     SELECT email
         AS 'Email'
+      FROM admin
+     WHERE admin.email = filter;
+
+
+  END
+;;
+
+DELIMITER ;
+
+-- Procedure get_password_of_admin()
+
+DROP PROCEDURE IF EXISTS get_password_of_admin;
+
+DELIMITER ;;
+
+CREATE PROCEDURE get_password_of_admin(
+                    `filter` VARCHAR(100)
+)
+ BEGIN
+
+    SELECT password
+        AS 'password'
+      FROM admin
+     WHERE admin.email = filter;
+
+
+  END
+;;
+
+DELIMITER ;
+
+-- Procedure check_if_admin_is_super()
+
+DROP PROCEDURE IF EXISTS check_if_admin_is_super;
+
+DELIMITER ;;
+
+CREATE PROCEDURE check_if_admin_is_super(
+                    `filter` VARCHAR(100)
+)
+ BEGIN
+
+    SELECT super
+        AS 'super'
       FROM admin
      WHERE admin.email = filter;
 
