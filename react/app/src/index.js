333e0db0043aa09ae7dd246da826ed17d66411ee
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-05 22:34:05 +0100

    Add App_user as root

--- react/app/src/index.js
+++ react/app/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Map from './components/Map';
-import App from './App_layerstack';
+//import Map from './components/Map';
+//import App from './App_layerstack';
+import App from './App_user';
 //import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <Map /> */}
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <Map /> */}
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
