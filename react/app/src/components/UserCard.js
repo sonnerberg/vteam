2cba403acbe5133c1c70a0a0cb2e75a7e5683236
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-05 22:35:26 +0100

    Add component for displaying user info

---
+++ react/app/src/components/UserCard.js
@@ -0,0 +1,57 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+/**
+ * A card with content
+ * @param {object} props - Props for the function
+ * @param {object} props.content Object with content
+ * @returns {React.ReactElement} - The card
+ */
+const UserCard = (props) => {
+    const rows = [];
+
+    for (const property in props.content) {
+        rows.push({
+            name: property,
+            value: props.content[property],
+        });
+    }
+
+    return (
+        <Card sx={{ minWidth: 200 }}>
+            <CardContent>
+                <Table sx={{ minWidth: 200 }} aria-label="simple table">
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{
+                                    '&:last-child td, &:last-child th': {
+                                        border: 0,
+                                    },
+                                }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.value}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </CardContent>
+            <CardActions>
+                <div>{props.button}</div>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default UserCard;
