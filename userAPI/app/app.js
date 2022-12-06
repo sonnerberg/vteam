df12c4370bc4b409c1597806f3b1a290d45e11c7
tzwo <tomzwo@gmail.com>
2022-12-06 09:53:31 +0100

    Add route for a test

--- userAPI/app/app.js
+++ userAPI/app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { queryDatabase } = require('./database/mariadb');
 require('dotenv').config();
 
 const app = express();
@@ -18,6 +19,12 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World from express and nodemon!');
+});
+
+app.get('/mariadb', async (req, res) => {
+    const sql = 'SELECT * FROM customer';
+    const data = await queryDatabase(sql);
+    res.status(200).json(data);
 });
 
 module.exports = app;
