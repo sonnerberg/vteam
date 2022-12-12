fac9f845cc3001aca187492c7f0a6c71ba2556fe
patrikmalvenius <malvenius@gmail.com>
2022-12-12 15:06:58 +0100

    add basic login form

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import AppMap from './App_layerstack';
 import AppUser from './App_user';
+import LoginForm from './components/LoginForm';
 import ToplevelSwitch from './components/ToplevelSwitch';
 import { useState } from 'react';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [showUser, setShowUser] = useState(false);
-    return (
-        <div className="topdiv">
-            <ToplevelSwitch showUser={showUser} setShowUser={setShowUser} />
-            {showUser ? <AppUser /> : <AppMap />}
-        </div>
-    );
+    if (isLoggedIn) {
+        return (
+            <div>
+                <ToplevelSwitch showUser={showUser} setShowUser={setShowUser} />
+                {showUser ? <AppUser /> : <AppMap />}
+            </div>
+        );
+    } else {
+        return (
+            <div className="topdiv">
+                <LoginForm />
+            </div>
+        );
+    }
 }
 export default App;
