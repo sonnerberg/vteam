ee6704a2dd76505d6e896330584622badcab6c98
tzwo <tomzwo@gmail.com>
2022-12-12 19:23:38 +0100

    Add route to update bike position

--- userAPI/app/routes/v1/index.js
+++ userAPI/app/routes/v1/index.js
@@ -43,10 +43,22 @@ router.get('/bikes', async (req, res) => {
     const sql = 'SELECT * FROM bikes';
     const data = await queryDatabase(sql);
     // res.status(200).json(data);
-    res.status(200).json(sqlToJson(data));
+    res.status(200).json(sqlToGeoJson(data));
 });
 
-const sqlToJson = (sql) => {
+router.put('/bikes/position', async (req, res) => {
+    // TODO placeholder and stored procedure bugs out. why?
+    const bikeId = req.body.id;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    const sql = `UPDATE bikes
+         SET geometry = ST_PointFromText("POINT(${latitude} ${longitude})")
+         WHERE id=${bikeId}`;
+    const data = await updateBikePosition(sql);
+    res.json(data);
+});
+
+const sqlToGeoJson = (sql) => {
     const geoJson = sql.map((x) => {
         return {
             position: {
@@ -66,6 +78,19 @@ const sqlToJson = (sql) => {
         };
     });
     return geoJson;
+};
+
+const updateBikePosition = async (sql) => {
+    const data = await queryDatabase(sql);
+    if (data.error) {
+        return data;
+    }
+    return {
+        data: {
+            status: 200,
+            message: 'bike position updated',
+        },
+    };
 };
 
 module.exports = router;
