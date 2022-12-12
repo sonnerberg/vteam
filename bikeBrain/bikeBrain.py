021bb5c27ce5697826760d3b01e5b84ff971e4b1
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-12 17:01:47 +0100

    Add fixes by pre-commit

--- bikeBrain/bikeBrain.py
+++ bikeBrain/bikeBrain.py
@@ -1,6 +1,7 @@
 from random import seed
 from random import randrange
 import time
+import json
 
 # import requests
 
@@ -10,36 +11,31 @@ class Brain:
 
     def __init__(self, _id, session, start_time, position, battery_capacity):
         """Init"""
+        self._id = _id
+        self._session = session
+        self._start_time = start_time
         self._position = position
         self._battery_capacity = battery_capacity
+
         self._speed = 0
-        # self._acceleration_rate = 1
-        # self._brake_rate = 1
-        self._whole = True
-        self._charging = False
+        self._is_locked = True
+        self._is_whole = True
+        self._is_charging = False
         self._is_blocked = False
         self._is_rented = False
         self._is_warning_battery = False
         self._is_battery_depleted = False
-        self._id = _id
+
         self._battery_decrease = 5
-        self._breaking_lamp_probability = 5
-        self._breaking_tyre_probability = 3
-        self._report_interval = 10
-        self._is_locked = True
-        self._start_time = start_time
-        self._current_time = 0.0
-        self._log_start_position = position
-        self._log_end_position = position
-        self._log_start_time = 0.0
-        self._log_end_time = 0.0
-        self._current_user = None
-        self._session = session
+        self._breaking_probability = 5
+
+        self._journey_log_start_position = position
+        self._journey_log_start_time = 0.0
+
         self._default_report_interval = 10
         self._moving_report_interval = 5
-        # Replace with new status [2, 2]
-        # Maybe also a charging status [3, 3]?
-        # self._is_rentable = true
+
+        self._current_user = None
 
         seed(self._id)
 
@@ -83,69 +79,21 @@ class Brain:
         """Sets speed"""
         self._speed = speed
 
-    def get_acceleration_rate(self):
-        """Gets acceleration rate"""
-        return self._acceleration_rate
-
-    def set_acceleration_rate(self, acceleration_rate):
-        """Sets acceleration rate"""
-        self._acceleration_rate = acceleration_rate
-
-    def increase_speed(self):
-        """Increase speed"""
-        self.setSpeed(self.get_speed() + self.get_acceleration_rate)
-
-    def get_brake_rate(self):
-        """Gets brake rate"""
-        return self._brake_rate
-
-    def set_brake_rate(self, brake_rate):
-        """Sets brake rate"""
-        self._brake_rate = brake_rate
-
-    def brake(self):
-        """Decrease speed"""
-        self.setSpeed(self.get_speed() - self.get_brake_rate)
-
-    def stop(self):
-        """Full stop"""
-        self.set_speed(0)
-
-    def get_log_start_position(self):
+    def get_journey_log_start_position(self):
         """Get start position"""
-        return self._log_start_position
+        return self._journey_log_start_position
 
-    def set_log_start_position(self, position):
+    def set_journey_log_start_position(self, position):
         """Set log for startposition"""
-        self._log_start_position = position
+        self._journey_log_start_position = position
 
-    def get_log_end_position(self):
-        """Get start position"""
-        return self._log_end_position
-
-    def set_log_end_position(self, position):
-        """Set log for startposition"""
-        self._log_end_position = position
-
-    def set_log_start_time(self, time):
+    def set_journey_log_start_time(self, time):
         """Set log start time"""
-        self._log_start_time = time
+        self._journey_log_start_time = time
 
-    def get_log_start_time(self):
+    def get_journey_log_start_time(self):
         """Get log start time"""
-        return self._log_start_time
-
-    def set_log_end_time(self, time):
-        """Set log start time"""
-        self._log_end_time = time
-
-    def get_log_end_time(self):
-        """Get log start time"""
-        return self._log_end_time
-
-    def report_status(self):
-        """Reports status"""
-        # print(f"Id: {self.get_id()}, status: {self.get_status()}")
+        return self._journey_log_start_time
 
     def set_current_user(self, user_id):
         """Set current user"""
@@ -164,14 +112,6 @@ class Brain:
         """Get start time"""
         return self._start_time
 
-    def set_current_time(self, a_time):
-        """Set current time"""
-        self._current_time = a_time
-
-    def get_current_time(self):
-        """Get current time"""
-        return self._current_time
-
     def get_report_interval(self):
         """Gets report interval"""
         return self._report_interval
@@ -180,6 +120,7 @@ class Brain:
         """Sets report interval"""
         self._report_interval = value
 
+    # Statuses
     def get_is_whole(self):
         """Gets is_whole"""
         return self._is_whole
@@ -234,7 +175,7 @@ class Brain:
         self._is_rented = status
         self._position["geometry"]["properties"]["rented"] = status
 
-    # Consider only have one probability for breaking, dont specify tyres and lamps
+    # Is bike breaking down during move?
     async def check_health(self):
         """Check health"""
         if self.get_battery_capacity() < 20:
@@ -245,18 +186,14 @@ class Brain:
             self.set_is_battery_depleted(True)
             await self.lock()
 
-        if randrange(1, 100) <= self._breaking_tyre_probability:
+        if randrange(1, 100) <= self._breaking_probability:
+            print("Breaking tyre")
             self.set_speed(0)
             self.set_is_blocked(True)
             self.set_is_whole(False)
             await self.lock()
 
-        if randrange(1, 100) <= self._breaking_lamp_probability:
-            self.set_speed(0)
-            self.set_is_blocked(True)
-            self.set_is_whole(False)
-            await self.lock()
-
+    # Move the bike to new coordinates
     async def move(self, coordinates):
         """Moves the bike to new position"""
         if not self.get_is_locked():
@@ -269,56 +206,62 @@ class Brain:
         else:
             self.set_report_interval(self._moving_report_interval)
 
+    # Report bike's position
     async def report_position(self):
         """Reports position"""
-        self.set_current_time(time.time())
-        interval = self.get_current_time() - self.get_start_time()
+        interval = time.time() - self.get_start_time()
         if interval > self.get_report_interval():
-            self.set_start_time(self.get_current_time())
+            # Set start_time to begin counting against the interval again
+            self.set_start_time(time.time())
+
+            print(f"Bike {self.get_id()} reporting")
+
             position = self.get_position()
+
             payload = {"position": position}
-            # r = requests.put(f"http://server:3000/bikes/{self.get_id()}", json=payload)
+
             async with self._session.put(
                 f"http://server:3000/bikes/{self.get_id()}", json=payload
             ) as resp:
                 result = await resp.json()
                 # handle result eg. set status to blocked depending on
-                # selfs status or position or likewise set is rented
+                # selfs status or position
 
+            return payload
+
+    # Unlock the bike and set statuses for new journey
     def unlock(self, user_id):
         """Unlock"""
-        # if self._is_rentable:
         if not self.get_is_blocked():
             self.set_is_locked(False)
             self.set_report_interval(self._moving_report_interval)
-            self.set_log_start_time(time.time())
-            self.set_log_start_position(self.get_position())
+            self.set_journey_log_start_time(time.time())
+            pos_dict = json.loads(json.dumps(self.get_position()))
+            self.set_journey_log_start_position(pos_dict)
             self.set_current_user(user_id)
             self.set_rented(True)
 
+    # Lock the bike and report the journey
     async def lock(self):
         """Lock"""
         self.set_is_locked(True)
         self.set_rented(False)
         self.set_report_interval(self._default_report_interval)
-        self.set_log_end_time(time.time())
-        self.set_log_end_position(self.get_position())
-        start_position = self.get_log_start_position()
-        end_position = self.get_position()
-        start_time = self.get_start_time()
-        end_time = time.time()
+
         payload = {
-            "startpostion": start_position,
-            "endposition": end_position,
-            "starttime": start_time,
-            "endtime": end_time,
+            "startpostion": self.get_journey_log_start_position(),
+            "endposition": self.get_position(),
+            "starttime": self.get_journey_log_start_time(),
+            "endtime": time.time(),
             "user-id": self.get_current_user(),
             "bike-id": self.get_id(),
         }
+
         async with self._session.post(
             f"http://server:3000/trips/", json=payload
         ) as resp:
             result = await resp.json()
             # Handle result if necessary
+
         self.set_current_user(None)
         print("Travel done")
