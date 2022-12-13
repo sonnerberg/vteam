e9d35ab727dd6a036989c6832f8c28b0e0eacd9b
tzwo <tomzwo@gmail.com>
2022-12-13 14:23:50 +0100

    Add bikes route

--- adminAPI/app/v1/routes/bikes.js
+++ adminAPI/app/v1/routes/bikes.js
@@ -3,39 +3,62 @@ const { queryDatabase } = require('../../database/mariadb');
 // const { authMiddleware } = require('../middleware/authMiddleware');
 
 const router = express.Router();
-const routeName = '/bikes';
+// const routeName = '/bikes';
 
-router.get(
-    `${routeName}/`,
-    // authMiddleware,
-    getAllBikes
-);
+router.get('/bikes', async (req, res) => {
+    const sql = 'SELECT * FROM bikes';
+    const data = await queryDatabase(sql);
+    // res.status(200).json(data);
+    res.status(200).json(sqlToGeoJson(data));
+});
 
-async function getAllBikes(req, res) {
-    try {
-        const sql = 'CALL get_bikes()';
-        const { 0: data } = await queryDatabase(sql);
+router.put('/bikes/position', async (req, res) => {
+    // TODO placeholder and stored procedure bugs out. why?
+    const bikeId = req.body.id;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    const sql = `UPDATE bikes
+         SET geometry = ST_PointFromText("POINT(${latitude} ${longitude})")
+         WHERE id=${bikeId}`;
+    const data = await updateBikePosition(sql);
+    res.json(data);
+});
 
-        res.status(200).json({
-            data,
-        });
-    } catch (error) {
-        switch (error) {
-        case 'InvalidEmailOrPassword':
-            res.sendStatus(400);
-            break;
-        case 'DuplicateEntry':
-            res.sendStatus(409);
-            break;
-        case 'NotSuperUser':
-            res.sendStatus(401);
-            break;
+const sqlToGeoJson = (sql) => {
+    const geoJson = sql.map((x) => {
+        return {
+            position: {
+                type: 'Feature',
+                geometry: x.geometry,
+                properties: {
+                    id: x.id,
+                    charging: x.charging,
+                    blocked: x.blocked,
+                    batteryWarning: x.battery_warning,
+                    batteryDepleted: x.battery_depleted,
+                    rented: x.rented,
+                    userId: x.user_id,
+                    featureType: 'bikes',
+                },
+            },
+        };
+    });
+    return {
+        data: geoJson,
+    };
+};
 
-        default:
-            res.sendStatus(500);
-            break;
-        }
+const updateBikePosition = async (sql) => {
+    const data = await queryDatabase(sql);
+    if (data.error) {
+        return data;
     }
-}
+    return {
+        data: {
+            status: 200,
+            message: 'bike position updated',
+        },
+    };
+};
 
 module.exports = router;
