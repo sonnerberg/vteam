027e6581a6dc36e07e11bc208e3a0ae0e67de642
patrikmalvenius <malvenius@gmail.com>
2022-12-16 19:18:37 +0100

    fetch bikes from real backend

--- customerapp/src/models/getFeatures.js
+++ customerapp/src/models/getFeatures.js
@@ -1,41 +1,42 @@
-const baseUrl = 'http://localhost:4000';
+const baseUrl = "http://localhost:4000";
+const backendUrl = "http://localhost:8081/v1/";
 
 const getFeatures = {
-    getCities: async function getCities() {
-        const response = await fetch(`${baseUrl}/cities`);
-        const result = await response.json();
-        return result;
-    },
-    getChargingStations: async function getChargingStations() {
-        const response = await fetch(`${baseUrl}/charging-stations`);
-        const result = await response.json();
-        return result;
-    },
-    getParkingLots: async function getParkingLots() {
-        const response = await fetch(`${baseUrl}/parking-lots`);
-        const result = await response.json();
-        return result;
-    },
-    getBikes: async function getBikes() {
-        const response = await fetch(`${baseUrl}/bikes`);
-        const result = await response.json();
-        return result;
-    },
-    getWorkshops: async function getWorkshops() {
-        const response = await fetch(`${baseUrl}/workshops`);
-        const result = await response.json();
-        return result;
-    },
-    getZones: async function getZones() {
-        const response = await fetch(`${baseUrl}/zones`);
-        const result = await response.json();
-        return result;
-    },
-    getPoints: async function getPoints() {
-        const response = await fetch(`${baseUrl}/points`);
-        const result = await response.json();
-        return result;
-    },
+  getCities: async function getCities() {
+    const response = await fetch(`${baseUrl}/cities`);
+    const result = await response.json();
+    return result;
+  },
+  getChargingStations: async function getChargingStations() {
+    const response = await fetch(`${baseUrl}/charging-stations`);
+    const result = await response.json();
+    return result;
+  },
+  getParkingLots: async function getParkingLots() {
+    const response = await fetch(`${baseUrl}/parking-lots`);
+    const result = await response.json();
+    return result;
+  },
+  getBikes: async function getBikes() {
+    const response = await fetch(`${backendUrl}/bikes`);
+    const result = await response.json();
+    return result.data;
+  },
+  getWorkshops: async function getWorkshops() {
+    const response = await fetch(`${baseUrl}/workshops`);
+    const result = await response.json();
+    return result;
+  },
+  getZones: async function getZones() {
+    const response = await fetch(`${baseUrl}/zones`);
+    const result = await response.json();
+    return result;
+  },
+  getPoints: async function getPoints() {
+    const response = await fetch(`${baseUrl}/points`);
+    const result = await response.json();
+    return result;
+  },
 };
 
 export default getFeatures;
