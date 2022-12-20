8ee2e4e8a3f9b0dcbed8464b0a043d24ddef4b0a
tzwo <tomzwo@gmail.com>
2022-12-20 22:07:13 +0100

    Another try to fix cors

--- userAPI/app/app.js
+++ userAPI/app/app.js
@@ -1,21 +1,28 @@
 const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const { queryDatabase } = require('./database/mariadb');
+const v1 = require('./routes/v1/index.js');
+const v2 = require('./routes/v2/index.js');
+const auth = require('./routes/auth/index.js');
 require('dotenv').config();
 
 const app = express();
 
-// app.use(cors());
-// app.options('*', cors());
-// // app.disable('x-powered-by');
+app.use(cors());
+app.options('*', cors());
+// app.disable('x-powered-by');
 
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: false,
-//     })
-// );
-// app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
+
+app.use('/v1', v1);
+app.use('/v2', v2);
+app.use('/auth', auth);
 
 app.get('/', (req, res) => {
     res.send('Hello World from express and nodemon!');
--- userAPI/app/index.js
+++ userAPI/app/index.js
@@ -1,27 +1,26 @@
 const PORT = 3000;
 const app = require('./app'); // the actual Express application
 const http = require('http');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// const cors = require('cors');
 
-const v1 = require('./routes/v1/index.js');
-const v2 = require('./routes/v2/index.js');
-const auth = require('./routes/auth/index.js');
+// const v1 = require('./routes/v1/index.js');
+// const v2 = require('./routes/v2/index.js');
+// const auth = require('./routes/auth/index.js');
 
-app.use(cors());
-app.options('*', cors());
-// app.disable('x-powered-by');
+// app.use(cors());
+// app.options('*', cors());
+// // app.disable('x-powered-by');
 
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
-);
-app.use(bodyParser.json());
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: false,
+//     })
+// );
+// app.use(bodyParser.json());
 
-app.use('/v1', v1);
-app.use('/v2', v2);
-app.use('/auth', auth);
+// app.use('/v1', v1);
+// app.use('/v2', v2);
+// app.use('/auth', auth);
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
