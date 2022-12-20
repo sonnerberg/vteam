4a8af758f9182ce82cb9f9aa9f467a043a48df37
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-20 15:41:20 +0100

    Add function to fetch user trips

--- customerapp/src/models/getUserData.js
+++ customerapp/src/models/getUserData.js
@@ -1,11 +1,24 @@
 const baseUrl = "http://localhost:4000";
 
 const getUserData = {
-  getUser: async function getUser(user) {
-    const response = await fetch(`${baseUrl}/users/${user}`);
+  getUser: async function getUser(user, token) {
+    const response = await fetch(`${baseUrl}/users/${user}`, {
+      headers: {
+        "x-access-token": token,
+      },
+    });
     const result = await response.json();
     console.log(result);
     return result;
+  },
+  getTripsByUserName: async function getTripsByUserName(username, token) {
+    const response = await fetch(`${baseUrl}/trips/${username}`, {
+      headers: {
+        "x-access-token": token,
+      },
+    });
+    const result = await response.json();
+    return result.data;
   },
 };
 
