6c3edd2cc5e26cbb9ce390591fd9a904c878157f
tzwo <tomzwo@gmail.com>
2022-12-20 21:54:11 +0100

    Change cors

--- userAPI/app/app.js
+++ userAPI/app/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
 const { queryDatabase } = require('./database/mariadb');
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
-app.options('*', cors());
-// app.disable('x-powered-by');
+// app.use(cors());
+// app.options('*', cors());
+// // app.disable('x-powered-by');
 
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
-);
-app.use(bodyParser.json());
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: false,
+//     })
+// );
+// app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World from express and nodemon!');
--- userAPI/app/index.js
+++ userAPI/app/index.js
@@ -1,10 +1,23 @@
 const PORT = 3000;
 const app = require('./app'); // the actual Express application
 const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const v1 = require('./routes/v1/index.js');
 const v2 = require('./routes/v2/index.js');
 const auth = require('./routes/auth/index.js');
+
+app.use(cors());
+app.options('*', cors());
+// app.disable('x-powered-by');
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
 
 app.use('/v1', v1);
 app.use('/v2', v2);
