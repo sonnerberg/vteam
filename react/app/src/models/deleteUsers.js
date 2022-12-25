4524a1cb797b4e463c0f03882db8dc8797c14912
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-25 10:46:50 +0100

    Add model for deleting users

---
+++ react/app/src/models/deleteUsers.js
@@ -0,0 +1,44 @@
+const baseUrl = 'http://localhost:8081/v1/';
+
+//TODO connect to real backend
+
+const deleteUsers = {
+    deleteCustomers: async function deleteCustomers(username, token) {
+        const data = {
+            username: username,
+        };
+        const bodyData = JSON.stringify(data);
+        const response = await fetch(`${baseUrl}`, {
+            body: bodyData,
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            method: 'DELETE',
+        });
+
+        const result = await response.json();
+
+        return result;
+    },
+    deleteAdmins: async function deleteAdmins(username, token) {
+        const data = {
+            username: username,
+        };
+        const bodyData = JSON.stringify(data);
+        const response = await fetch(`${baseUrl}`, {
+            body: bodyData,
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            method: 'DELETE',
+        });
+
+        const result = await response.json();
+
+        return result;
+    },
+};
+
+export default deleteUsers;
