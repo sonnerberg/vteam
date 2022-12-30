3dbbb9d83e975a158d702e312d93259e534c076d
Joel Löfgren <jdtlofgren@gmail.com>
2022-12-30 16:52:54 +0100

    Change test for app.js

--- customerapp/src/App.test.js
+++ customerapp/src/App.test.js
@@ -1,17 +1,67 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event/";
+import getUserData from "./models/getUserData";
+import getFeatures from "./models/getFeatures";
 
-test("renders links", () => {
-  render(<App />);
-  const map = screen.getByText(/karta/i);
-  const account = screen.getByText(/konto/i);
-  expect(map).toBeInTheDocument();
-  expect(account).toBeInTheDocument();
+// Mock fetch to return a token
+let originalFetch;
+/*
+beforeEach(() => {
+  originalFetch = global.fetch;
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          status: 200,
+          message: "successful login",
+          token: "1",
+          user: "user",
+        }),
+    })
+  );
 });
 
-/* test("login", async () => {
-  render(<App />);
-  const login = screen.getByText(/ta mig till github/i);
-  const user = userEvent.setup();
-  await user.click(login);
+afterEach(() => {
+  global.fetch = originalFetch;
 }); */
+
+/* beforeEach(() => {
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: jest.fn().mockResolvedValue({
+      status: 200,
+      message: "successful login",
+      token: "1",
+      user: "user",
+    }),
+  });
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+test("login", async () => {
+  getUserData.getUser = jest.fn();
+  getUserData.getTripsByUserName = jest.fn();
+  getFeatures.getCities = jest.fn();
+  getFeatures.getChargingStations = jest.fn();
+  getFeatures.getParkingLots = jest.fn();
+  getFeatures.getBikes = jest.fn();
+  getFeatures.getCities = jest.fn();
+
+  render(<App />);
+  const user = userEvent.setup();
+  const login = await screen.findByText(/ta mig till github/i);
+  await user.click(login);
+  const account = await screen.findByText(/konto/i);
+  await user.click(account);
+  await screen.findByText(/uppgifter/i);
+  screen.debug();
+}); */
+
+test("App renders", async () => {
+  render(<App />);
+  const login = await screen.findByText(/ta mig till github/i);
+  expect(login).toBeInTheDocument();
+});
