6067eb40574289395cb121e5880e3847673bfc6a
Joel Löfgren <jdtlofgren@gmail.com>
2023-01-03 12:49:17 +0100

    Add token to props of formcard

--- react/app/src/components/LayerAccordion.js
+++ react/app/src/components/LayerAccordion.js
@@ -132,6 +132,7 @@ const LayerAccordion = (props) => {
                         drawnItems={props.drawnItems}
                         triggerRedraw={props.triggerRedraw}
                         setTriggerRedraw={props.setTriggerRedraw}
+                        token={props.token}
                     />
                 );
                 setCard(newCard);
